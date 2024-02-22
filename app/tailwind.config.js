c227aea7325a6a02ee5c1da44b0c9fc09c745fc4
190101101 <190101101@mail.ru>
2024-02-22 09:38:59 +0400

    03 chat app\01 main content

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,17 +1,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        "main-grey": "#14171a",
+        "main-green": "#8ac832",
+        "main-dark-green": "#28a745",
+        "main-orange": "#e84118",
+      },
+      fontFamily: {
+        consolas: ["Consolas", "courier new"],
+      },
       container: {
         center: true,
         screens: {
-          lg: '1140px',
-          xl: '1140px',
-          '2xl': '1140px',
-        }
+          lg: "1140px",
+          xl: "1140px",
+          "2xl": "1140px",
+        },
       },
-    }
+      spacing: {
+        128: "32rem",
+      },
+    },
   },
   plugins: [],
-}
+};
